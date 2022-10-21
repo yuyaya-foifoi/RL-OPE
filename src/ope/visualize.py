804dc8294e74bb45b41d1fe4ede9afe0e2d35638
yuyaya-foifoi <fujisaki.for.2018@gmail.com>
2022-10-21 17:16:48 +0900

    experiment 1021

--- src/ope/visualize.py
+++ src/ope/visualize.py
@@ -2,23 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def visualize_error_per_distance(opes: np.array, model_name: list):
+def visualize_plot(
+    x: np.array,
+    y: np.array,
+    yerr: np.array,
+    model_name: list,
+    is_errorbar: bool,
+    labels: list,
+):
 
-    x = opes[:, 0]
-    y = np.abs(np.mean(opes[:, 1:], axis=1))
+    if is_errorbar:
+        plt.errorbar(
+            x,
+            y,
+            yerr=yerr,
+            capsize=4,
+            fmt="o",
+            ecolor="red",
+            color="black",
+        )
+        for i, label in enumerate(model_name):
+            plt.annotate(model_name[i], (x[i], y[i]))
+        plt.xlabel(labels[0])
+        plt.ylabel(labels[1])
+        plt.show()
+        plt.close()
 
-    plt.errorbar(
-        x,
-        y,
-        yerr=np.std(opes[:, 1:], axis=1),
-        capsize=4,
-        fmt="o",
-        ecolor="red",
-        color="black",
-    )
-    for i, label in enumerate(model_name):
-        plt.annotate(model_name[i], (x[i], y[i]))
-    plt.xlabel("distribution distance")
-    plt.ylabel("value error")
-    plt.show()
-    plt.close()
+    if not is_errorbar:
+        plt.scatter(
+            x,
+            y,
+        )
+        for i, label in enumerate(model_name):
+            plt.annotate(model_name[i], (x[i], y[i]))
+        plt.xlabel(labels[0])
+        plt.ylabel(labels[1])
+        plt.show()
+        plt.close()
